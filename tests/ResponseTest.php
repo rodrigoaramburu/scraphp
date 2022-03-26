aa941ee9616f7f8586cec0b117b4a5985d24ed53
Rodrigo Aramburu <rodrigoaramburu@gmail.com>
2022-03-26 13:01:29 -0300

    fix: alterado os mocks do ResponseTest para Mockery

--- tests/ResponseTest.php
+++ tests/ResponseTest.php
@@ -2,14 +2,16 @@
 
 declare(strict_types=1);
 
+use Mockery\Mock;
 use ScraPHP\Response;
 use ScraPHP\HttpClient\HttpClientInterface;
 
 
 test('deve chamar filtro css do httpclient', function(){
 
-    $httpClient = $this->createMock(HttpClientInterface::class);
-    $httpClient->expects( $this->once())->method('css')->with('.seletor')->willReturn(null);
+    /** @var Mock|HttpClientInterface */
+    $httpClient = Mockery::mock(HttpClientInterface::class);
+    $httpClient->shouldReceive('css')->once()->with('.seletor')->andReturn(null);
 
     $response = new Response(
         url: 'http://example.com',
@@ -22,14 +24,15 @@ test('deve chamar filtro css do httpclient', function(){
     expect($value)->toBeNull();
 });
 
-test('deve chamar  cssEach do httpclient', function(){
+test('deve chamar cssEach do httpclient', function(){
 
     $closure = function($element){
         return [1,2,3];
     };
 
-    $httpClient = $this->createMock(HttpClientInterface::class);
-    $httpClient->expects( $this->once())->method('cssEach')->with('.seletor', $closure)->willReturn([1,2,3]);
+    /** @var Mock|HttpClientInterface */
+    $httpClient = Mockery::mock(HttpClientInterface::class);
+    $httpClient->shouldReceive('cssEach')->once()->with('.seletor', $closure)->andReturn([1,2,3]);
 
     $response = new Response(
         url: 'http://example.com',
